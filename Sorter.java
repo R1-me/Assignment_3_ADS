9a4745cdedb59a345dbd8aefcb4759494d501864
Amir <kunanbayevamir@gmail.com>
2026-04-28 21:14:19 +0500

    implemented basic and advanced sorting algorithms

---
+++ Sorter.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+
+public class Sorter {
+// Insertion Sort
+    public void basicSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+// Merge Sort
+    public void advancedSort(int[] arr) {
+        if (arr.length <= 1) return;
+
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+
+        for (int i = 0; i < mid; i++) left[i] = arr[i];
+        for (int i = mid; i < arr.length; i++) right[i - mid] = arr[i];
+
+        advancedSort(left);
+        advancedSort(right);
+
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) arr[k++] = left[i++];
+            else arr[k++] = right[j++];
+        }
+        while (i < left.length) arr[k++] = left[i++];
+        while (j < right.length) arr[k++] = right[j++];
+    }
+
+    public void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public int[] generateRandomArray(int size) {
+        int[] arr = new int[size];
+        Random r = new Random();
+        for (int i = 0; i < size; i++) {
+            arr[i] = r.nextInt(10000);
+        }
+        return arr;
+    }
+
+    public int[] generateSortedArray(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = i;
+        }
+        return arr;
+    }
+}
